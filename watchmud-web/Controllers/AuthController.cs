13675fda72422a03283564ffaaf2021f63f83687
Tony Rasa <trasa@meancat.com>
2020-10-08 11:29:30 -0700

    added razor sample pages back in
    index has github login button and fixed oauth redirect

--- watchmud-web/Controllers/AuthController.cs
+++ watchmud-web/Controllers/AuthController.cs
@@ -7,11 +7,11 @@ namespace Watchmud.Web.Controllers
     public class AuthController : Controller
     {
         [HttpGet]
-        public IActionResult Login(string returnUri = "/")
+        public IActionResult Login()
         {
             return Challenge(new AuthenticationProperties
             {
-                RedirectUri = returnUri
+                RedirectUri = "https://localhost:11000/github-oauth"
             });
         }
     }
---
+++ watchmud-web/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Octokit;
+using Octokit.Internal;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        
+        public IReadOnlyList<Repository> Repositories { get; set; }
+        public IReadOnlyList<Repository> StarredRepositories { get; set; }
+        public IReadOnlyList<User> Followers { get; set; }
+        public IReadOnlyList<User> Following { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"), new InMemoryCredentialStore(new Credentials(accessToken)));
+                Repositories = await github.Repository.GetAllForCurrent();
+                StarredRepositories = await github.Activity.Starring.GetAllForCurrent();
+                Followers = await github.User.Followers.GetAllForCurrent();
+                Following = await github.User.Followers.GetAllFollowingForCurrent();
+            }
+        }
+    }
+}
