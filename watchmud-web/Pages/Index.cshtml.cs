ca1be4d381d01fb4fe00d2796e009f0c3443932c
Tony Rasa <trasa@meancat.com>
2020-10-12 11:32:43 -0700

    added google login via oauth2

--- watchmud-web/Pages/Index.cshtml.cs
+++ watchmud-web/Pages/Index.cshtml.cs
@@ -1,16 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Octokit;
 using Octokit.Internal;
 
 namespace WebApplication1.Pages
 {
+    /*
+     * res: {
+"id": "102171105725265301028",
+"email": "trasa@meancat.com",
+"verified_email": true,
+"name": "Tony Rasa",
+"given_name": "Tony",
+"family_name": "Rasa",
+"link": "https://plus.google.com/102171105725265301028",
+"picture": "https://lh3.googleusercontent.com/a-/AOh14GhjQ9PK4zNgPtf8jPFLdPKIXKksetebiqAYVnnTzg=s96-c",
+"gender": "male",
+"locale": "en",
+"hd": "meancat.com"
+}
+     */
+    public class GoogleUserInfo
+    {
+        public string Id { get; set; }
+        public string Email { get; set;}
+        
+        [JsonProperty("verified_email")]
+        public bool VerifiedEmail { get; set; }
+        
+        public string Name { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Link { get; set; }
+        public string Picture { get; set; }
+        public string Gender { get; set; }
+        public string Locale { get; set; }
+        public string HD { get; set; }
+    }
+    
+    
     public class EmailModel {
         public string Email { get; set; }
         public bool Verified { get; set; }
@@ -25,7 +66,7 @@ namespace WebApplication1.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> logger;
-        public string GitHubUserPublicEmail { get; set; }
+        public string UserPublicEmail { get; set; }
         public IEnumerable<EmailModel> Emails { get; set; }
         public string AuthenticationSource { get; set; }
 
@@ -39,13 +80,13 @@ namespace WebApplication1.Pages
             if (User.Identity.IsAuthenticated)
             {
                 string accessToken = await HttpContext.GetTokenAsync("access_token");
-                AuthenticationSource = User.Identity.AuthenticationType;
+                this.AuthenticationSource = User.Identity.AuthenticationType;
                 if (User.Identity.AuthenticationType == "GitHub")
                 {
                     await GitHubAsync(accessToken);
                 } else if (User.Identity.AuthenticationType == "Google")
                 {
-                    GoogleAsync(accessToken);
+                    await GoogleAsync(accessToken);
                 }
             }
         }
@@ -61,7 +102,7 @@ namespace WebApplication1.Pages
             var user = await github.User.Current();
             // if the user has a public email set, then this shows here:
             logger.LogInformation($"user github public email is {user.Email}");
-            GitHubUserPublicEmail = user.Email;
+            UserPublicEmail = user.Email;
 
             var emails = await github.User.Email.GetAll();
             logger.LogInformation($"email count {emails.Count}");
@@ -75,10 +116,30 @@ namespace WebApplication1.Pages
                 };
         }
 
-        private void GoogleAsync(string accessToken)
+        private async Task GoogleAsync(string accessToken)
         {
-            logger.LogInformation("do google async here");
-            GitHubUserPublicEmail = "google auth stuff here";
+            // TODO use dependency injection
+            using (var client = new HttpClient())
+            {
+                // I couldn't find a way to do this call from within the Google C# Lib and
+                // avoid using HttpClient and JSON deserialization. It's likely that there's 
+                // already a helper method somewhere that does this...
+                // Elsewise this should be wrapped into some sort of helper service and use
+                // DI and all that other good stuff.
+                string json = await client.GetStringAsync("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + accessToken);
+                GoogleUserInfo userInfo = JsonConvert.DeserializeObject<GoogleUserInfo>(json);
+                UserPublicEmail = userInfo.Email;
+                Emails = new List<EmailModel>
+                {
+                    new EmailModel
+                    {
+                        Email = userInfo.Email,
+                        Verified = userInfo.VerifiedEmail,
+                        Primary = true,
+                        Visibility = "N/A",
+                    }
+                };
+            }
         }
     }
 }
